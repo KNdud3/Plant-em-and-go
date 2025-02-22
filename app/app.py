2f196e56449cc43def6c98680f760d3aac1c0732
Watheeq Rizvi <wrizvi2020@gmail.com>
2025-02-22 15:41:25 +0000

    copied plant api code to main branch

--- app/app.py
+++ app/app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template
+import requests
+import json
 
 
 app = Flask(__name__, template_folder='static')
@@ -18,6 +20,22 @@ def login():
 @app.route("/Register")
 def register():
     return render_template("Register.html")
+
+
+@app.route("/testPlantAPI")
+def testPlantAPI():
+    # Make request to plant API and return a JSON string with correct information
+    api_url = "https://my-api.plantnet.org/v2/identify/all?nb-results=1&api-key=2b10sbrhApe42g9nJ0ypm2lcO"
+    image = open("../assets/dandelion.jpg", 'rb')
+    file = [('images', image)]
+    response = requests.post(api_url, files=file, data = {})
+    result = response.json()
+
+    # Parse JSON
+    speciesName = (result['bestMatch'])
+    commonName = (result['results'][0]['species']['commonNames'][0])
+    return ("Species: {} <br> Common Name = {} <br> <br> <br> Results: <br> {}".format(speciesName,commonName,result))
+
 
 if __name__ == "__main__":
     app.run(debug=True)
